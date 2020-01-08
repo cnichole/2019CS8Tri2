107010d1b09f1506315d3648f429293991b7d8af
cnichole <cfleming24@nscds.org>
2020-01-08 20:58:33 +0000

    Working on Star HTML and JS

---
+++ nightscence/star.js
@@ -0,0 +1,6 @@
+for(var c=0; c<10; c++){
+    var x = Math.floor((Math.random() * 1000) + 1);
+    var y = Math.floor((Math.random() * 1000) + 1);
+    $('body').append("<div class='star' style='top:43px ,left: 100px;'></div>");
+}
+        
