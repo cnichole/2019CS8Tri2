c3323f72433c009593cf2c860f50fa413a504dc1
cnichole <cfleming24@nscds.org>
2020-01-16 15:24:08 +0000

    FInished Stars. Starting on Shooting Star

--- nightscence/star.js
+++ nightscence/star.js
@@ -1,6 +1,6 @@
-for(var c=0; c<10; c++){
-    var x = Math.floor((Math.random() * 1000) + 1);
-    var y = Math.floor((Math.random() * 1000) + 1);
-    $('body').append("<div class='star' style='top:43px ,left: 100px;'></div>");
+for(var c=0; c<600; c++){
+    var x = Math.floor((Math.random() * 550) + 1);
+    var y = Math.floor((Math.random() * 1300) + 1);
+    $('body').prepend("<div class='star' style= 'top: "+x+"px; left: "+y+"px; '></div>");
 }
         
