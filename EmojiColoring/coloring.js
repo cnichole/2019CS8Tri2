7a68a87606fd26dfe6e0e89821bf9a174d7d9d20
cnichole <cfleming24@nscds.org>
2020-02-03 19:08:34 +0000

    Clicking

--- EmojiColoring/coloring.js
+++ EmojiColoring/coloring.js
@@ -1,11 +1,23 @@
 $(".pupilImage").click(
     function(){
-        alert("I wish it was Friday");
+    var clickedPupil = $(this).css("background-image")
+    $(".pupil").css("background-image",clickedPupil);
     }
 );
 
 $(".mouthImage").click(
     function(){
-        var clickedMouth= $(this).css("background-image");
+        var clickedMouth = $(this).css("background-image");
+        $("#mouth").css("background-image",clickedMouth);
     }
+);
+
+$(".color").click(
+    function(){
+        var color = $(this).css("background-color");
+        $("#face").css("background-color",color);
+
+    }
+
+
 );
