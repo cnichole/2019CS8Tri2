51c2e9e629566653ba4c3ebf5b034a1cbcaa9ddf
cnichole <cfleming24@nscds.org>
2020-02-12 19:06:49 +0000

    final project

--- Final/final.js
+++ Final/final.js
@@ -5,3 +5,10 @@ $(".sky").click(
     }
 );
 
+function startRace(){
+    TweenMax.to("#yellow",2,{x:520,ease:Sine.easeOut});
+    TweenMax.to("#yellow",1.5,{x:500, y:-320,delay:1.7, ease:Sine.easeOut});
+    TweenMax.to("#yellow",2,{x:-50, y:-300,delay:3, ease:Sine.easeOut});
+      
+}
+
